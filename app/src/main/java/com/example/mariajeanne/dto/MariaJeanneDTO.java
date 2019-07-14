eef7628ce8caf0d332ad729e2c2d26f347de7cf8
Anargyres <anargyres9278@gmail.com>
2019-07-14 16:32:55 +0200

    Ajout model

--- app/src/main/java/com/example/mariajeanne/dto/MariaJeanneDTO.java
+++ app/src/main/java/com/example/mariajeanne/dto/MariaJeanneDTO.java
@@ -1,4 +1,38 @@
 package com.example.mariajeanne.dto;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class MariaJeanneDTO {
+
+    @SerializedName("id") int id;
+    @SerializedName("race") String race;
+    @SerializedName("flavors")
+    List<String> flavors;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public List<String> getFlavors() {
+        return flavors;
+    }
+
+    public void setFlavors(List<String> flavors) {
+        this.flavors = flavors;
+    }
 }
--- app/src/main/java/com/example/mariajeanne/model/MariaJeanne.java
+++ app/src/main/java/com/example/mariajeanne/model/MariaJeanne.java
@@ -1,4 +1,34 @@
 package com.example.mariajeanne.model;
 
+import java.util.List;
+
 public class MariaJeanne {
+
+    int id;
+    String race;
+    List<String> flavor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public List<String> getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(List<String> flavor) {
+        this.flavor = flavor;
+    }
 }

e820025b565da6d0474ee4f57dcca49f465ba491
Anargyres <anargyres9278@gmail.com>
2019-07-14 16:26:58 +0200

    Ajout all get

--- app/src/main/java/com/example/mariajeanne/service/MariaJeanneService.java
+++ app/src/main/java/com/example/mariajeanne/service/MariaJeanneService.java
@@ -11,4 +11,16 @@ public interface MariaJeanneService {
 
     @GET("strains/search/all ")
     Call<List<MariaJeanneDTO>> getAllMJ();
+
+    @GET("strains/search/name/{name}")
+    Call<List<MariaJeanneDTO>> getMJByName(String name);
+
+    @GET("strains/search/race/{race}")
+    Call<List<MariaJeanneDTO>> getMJByRace(String race);
+
+    @GET("strains/search/effect/{effect}")
+    Call<List<MariaJeanneDTO>> getMJByEffect(String effect);
+
+    @GET("strains/search/flavor/{flavor}")
+    Call<List<MariaJeanneDTO>> getMJByFlavor(String flavor);
 }
--- app/src/main/java/com/example/mariajeanne/service/NetworkProvider.java
+++ app/src/main/java/com/example/mariajeanne/service/NetworkProvider.java
@@ -38,6 +38,74 @@ public class NetworkProvider {
         });
     }
 
+    public void getMJByName(final Listener<List<MariaJeanne>> listListener, String name){
+        mariaJeanneService.getMJByName(name).enqueue(new Callback<List<MariaJeanneDTO>>() {
+            @Override
+            public void onResponse(Call<List<MariaJeanneDTO>> call, Response<List<MariaJeanneDTO>> response) {
+                List<MariaJeanneDTO> mariaJeanneDTOList = response.body();
+                List<MariaJeanne> mariaJeanneList = MariaJeanneMapper.map(mariaJeanneDTOList);
+
+                listListener.onSuccess(mariaJeanneList);
+            }
+
+            @Override
+            public void onFailure(Call<List<MariaJeanneDTO>> call, Throwable t) {
+                listListener.onError(t);
+            }
+        });
+    }
+
+    public void getMJByRace(final Listener<List<MariaJeanne>> listListener, String race){
+        mariaJeanneService.getMJByRace(race).enqueue(new Callback<List<MariaJeanneDTO>>() {
+            @Override
+            public void onResponse(Call<List<MariaJeanneDTO>> call, Response<List<MariaJeanneDTO>> response) {
+                List<MariaJeanneDTO> mariaJeanneDTOList = response.body();
+                List<MariaJeanne> mariaJeanneList = MariaJeanneMapper.map(mariaJeanneDTOList);
+
+                listListener.onSuccess(mariaJeanneList);
+            }
+
+            @Override
+            public void onFailure(Call<List<MariaJeanneDTO>> call, Throwable t) {
+                listListener.onError(t);
+            }
+        });
+    }
+
+    public void getMJByEffect(final Listener<List<MariaJeanne>> listListener, String effect){
+        mariaJeanneService.getMJByEffect(effect).enqueue(new Callback<List<MariaJeanneDTO>>() {
+            @Override
+            public void onResponse(Call<List<MariaJeanneDTO>> call, Response<List<MariaJeanneDTO>> response) {
+                List<MariaJeanneDTO> mariaJeanneDTOList = response.body();
+                List<MariaJeanne> mariaJeanneList = MariaJeanneMapper.map(mariaJeanneDTOList);
+
+                listListener.onSuccess(mariaJeanneList);
+            }
+
+            @Override
+            public void onFailure(Call<List<MariaJeanneDTO>> call, Throwable t) {
+                listListener.onError(t);
+            }
+        });
+    }
+
+    public void getMJByFlavor(final Listener<List<MariaJeanne>> listListener, String flavor){
+        mariaJeanneService.getMJByFlavor(flavor).enqueue(new Callback<List<MariaJeanneDTO>>() {
+            @Override
+            public void onResponse(Call<List<MariaJeanneDTO>> call, Response<List<MariaJeanneDTO>> response) {
+                List<MariaJeanneDTO> mariaJeanneDTOList = response.body();
+                List<MariaJeanne> mariaJeanneList = MariaJeanneMapper.map(mariaJeanneDTOList);
+
+                listListener.onSuccess(mariaJeanneList);
+            }
+
+            @Override
+            public void onFailure(Call<List<MariaJeanneDTO>> call, Throwable t) {
+                listListener.onError(t);
+            }
+        });
+    }
+
     public interface Listener<T>{
         void onSuccess(T data);
 

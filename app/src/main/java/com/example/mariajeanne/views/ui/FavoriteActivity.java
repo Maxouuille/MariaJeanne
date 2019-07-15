5374a9fd589479604985c87111e42c0e3e999c8b
Anargyres <anargyres9278@gmail.com>
2019-07-15 13:14:37 +0200

    Tentative ajout connexion google

--- app/src/main/java/com/example/mariajeanne/views/ui/FavoriteActivity.java
+++ app/src/main/java/com/example/mariajeanne/views/ui/FavoriteActivity.java
@@ -1,21 +1,25 @@
 package com.example.mariajeanne.views.ui;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.example.mariajeanne.R;
 import com.example.mariajeanne.views.contract.IFavoriteActivity;
 
 public class FavoriteActivity extends AppCompatActivity implements IFavoriteActivity {
 
+    Button connectionBtn;
+    //GoogleSignInClient mGoogleSignInClient;
+
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.action_home:
                     Intent intentMain = new Intent(FavoriteActivity.this, MainActivity.class);
                     startActivity(intentMain);
@@ -37,6 +41,14 @@ public class FavoriteActivity extends AppCompatActivity implements IFavoriteActi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        /*connectionBtn = findViewById(R.id.activity_favorite_connection_btn);
+        connectionBtn.setOnClickListener(v -> {
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build();
+            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        });*/
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         bottomNavigationView.setSelectedItemId(R.id.action_favorite);
